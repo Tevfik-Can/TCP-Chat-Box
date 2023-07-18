c37111508878e096b4d20e4177fefd3a7ea04e39
Tevfik Gulbent <tcgulbent@gmail.com>
2023-07-18 16:41:27 -0400

    added comments

--- client.py
+++ client.py
@@ -10,24 +10,28 @@ PORT = 9090
 class Client:
 
     def __init__(self, host, port):
+        # Initialize the client's socket and connect to the server
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((host, port))
 
+        # Ask the user to enter their nickname using a pop-up dialog
         msg = tkinter.Tk()
         msg.withdraw()
-
         self.nickname = simpledialog.askstring("Nickname", "Enter your nickname", parent=msg)
 
+        # Set up variables for GUI and client running status
         self.gui_done = False
         self.running = True
 
+        # Create two separate threads for GUI and receiving messages
         gui_thread = threading.Thread(target=self.gui_loop)
         receive_thread = threading.Thread(target=self.receive)
 
+        # Start the threads
         gui_thread.start()
         receive_thread.start()
 
-    #Front end part
+    # Front-end part: GUI loop for the chat window
     def gui_loop(self):
         self.win = tkinter.Tk()
         self.win.configure(bg="gray")
@@ -36,9 +40,9 @@ class Client:
         self.chat_label.config(font=("Arial", 12))
         self.chat_label.pack(padx=20, pady=5)
 
-        #Chat history
+        # Chat history area using a scrolled text widget
         self.text_area = tkinter.scrolledtext.ScrolledText(self.win)
-        self.text_area.pack(padx=20,pady=5)
+        self.text_area.pack(padx=20, pady=5)
         self.text_area.config(state='disabled')
 
         self.msg_label = tkinter.Label(self.win, text="Message:", bg="gray")
@@ -46,7 +50,7 @@ class Client:
         self.msg_label.pack(padx=20, pady=5)
 
         self.input_area = tkinter.Text(self.win, height=3)
-        self.input_area.pack(padx=20,pady=5)
+        self.input_area.pack(padx=20, pady=5)
 
         self.send_button = tkinter.Button(self.win, text="Send!", command=self.write)
         self.send_button.config(font=("Arial", 12))
@@ -54,29 +58,34 @@ class Client:
 
         self.gui_done = True
 
+        # Handle the window close event
         self.win.protocol("WM_DELETE_WINDOW", self.stop)
 
         self.win.mainloop()
 
+    # Method to stop the client and close the connection
     def stop(self):
         self.running = False
         self.win.destroy()
         self.sock.close()
         exit(0)
 
+    # Method to send a message from the input area to the server
     def write(self):
         message = f"{self.nickname}: {self.input_area.get('1.0', 'end')}"
         self.sock.send(message.encode('utf-8'))
         self.input_area.delete('1.0', 'end')
 
-
+    # Method to receive messages from the server and update the chat window
     def receive(self):
         while self.running:
             try:
                 message = self.sock.recv(1024).decode('utf-8')
                 if message == "NICK":
+                    # The server is requesting the nickname from the client
                     self.sock.send(self.nickname.encode('utf-8'))
                 else:
+                    # If the GUI is ready, update the chat window with the received message
                     if self.gui_done:
                         self.text_area.config(state='normal')
                         self.text_area.insert('end', message)
@@ -85,8 +94,9 @@ class Client:
             except ConnectionAbortedError:
                 break
             except:
-                print("Error Occured!")
+                print("Error Occurred!")
                 self.sock.close()
                 break
 
+# Create a Client instance and start the chat client
 client = Client(HOST, PORT)
--- server.py
+++ server.py
@@ -3,31 +3,40 @@ import threading
 
 HOST = '127.0.0.1'
 PORT = 9090
+
+# Create a socket for the server using IPv4 address family and TCP protocol
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the server to the specified host and port
 server.bind((HOST, PORT))
 
+# Listen for incoming connections
 server.listen()
 
+# Lists to keep track of connected clients and their nicknames
 clients = []
 nicknames = []
 
-#broadcast
+# Function to broadcast a message to all connected clients
 def broadcast(message):
     for client in clients:
         client.send(message)
 
+# Function to handle the messages from a client
 def handle(client):
     while True:
         try:
+            # Receive messages from the client (up to 1024 bytes)
             message = client.recv(1024)
 
-            #logging message to server
+            # Log the received message to the server console
             print(f"{nicknames[clients.index(client)]} is saying: {message}")
 
-            #sending it to user
+            # Broadcast the message to all connected clients
             broadcast(message)
-            pass
         except:
+            # If an error occurs (client disconnects or an exception is raised),
+            # remove the client from the list and close the connection.
             index = clients.index(client)
             clients.remove(client)
             client.close()
@@ -36,27 +45,35 @@ def handle(client):
             nicknames.remove(nickname)
             break
 
-#receive
+# Function to receive incoming connections and start a thread to handle each client
 def receive():
     while True:
+        # Accept a new client connection
         client, address = server.accept()
         print(f"Connected with {str(address)}")
 
+        # Ask the client to send its nickname
         client.send("NICK".encode('utf-8'))
         nickname = client.recv(1024)
         nicknames.append(nickname)
 
+        # Add the client to the list of connected clients
         clients.append(client)
 
-        #Sends it to the server
+        # Print the client's nickname on the server console
         print(f"Nickname of the client is {nickname}")
 
-        #sends it to the chat room
+        # Broadcast a message to all clients notifying about the new connection
         broadcast(f"{nickname} has connected to the server".encode('utf-8'))
+
+        # Send a message to the newly connected client
         client.send(f"{nickname} has connected to the server\n".encode('utf-8'))
 
+        # Start a new thread to handle the client's messages
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
 
-print("=-=-=-=-=-=-= Server is Running =-=-=-=-=-=-=")
-receive()
+# Main function to start the server and wait for incoming connections
+if __name__ == "__main__":
+    print("=-=-=-=-=-=-= Server is Running =-=-=-=-=-=-=")
+    receive()
