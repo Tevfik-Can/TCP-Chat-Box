9fd0af351d0fa16e2d3439d084bc451cd113c01f
Tevfik Gulbent <tcgulbent@gmail.com>
2023-07-18 16:27:28 -0400

    first commit

---
+++ server.py
@@ -0,0 +1,62 @@
+import socket
+import threading
+
+HOST = '127.0.0.1'
+PORT = 9090
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+
+server.listen()
+
+clients = []
+nicknames = []
+
+#broadcast
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+
+            #logging message to server
+            print(f"{nicknames[clients.index(client)]} is saying: {message}")
+
+            #sending it to user
+            broadcast(message)
+            pass
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+
+            nickname = nicknames[index]
+            nicknames.remove(nickname)
+            break
+
+#receive
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f"Connected with {str(address)}")
+
+        client.send("NICK".encode('utf-8'))
+        nickname = client.recv(1024)
+        nicknames.append(nickname)
+
+        clients.append(client)
+
+        #Sends it to the server
+        print(f"Nickname of the client is {nickname}")
+
+        #sends it to the chat room
+        broadcast(f"{nickname} has connected to the server".encode('utf-8'))
+        client.send(f"{nickname} has connected to the server\n".encode('utf-8'))
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+print("=-=-=-=-=-=-= Server is Running =-=-=-=-=-=-=")
+receive()
